149f55991d31d56fcd64189c402c22b3ea749686
James T. Perreault <jperreault@terakeet.com>
2022-10-19 22:46:52 -0400

    refactor: rename the src folder

--- hashami/hasher.py
+++ hashami/hasher.py
@@ -2,10 +2,10 @@
 import hashlib
 
 
-def dict_hasher(_dict):
+def hash_dict(_dict):
     """sort, convert dict object to a URI-like string, hash it using SHA1"""
     string_list = []
     for tup in sorted(_dict.items()):
-        string_list.append('='.join(tup))
+        string_list.append('='.join(str(tup)))
 
     return hashlib.sha1('&'.join(string_list).encode('utf-8')).hexdigest()
